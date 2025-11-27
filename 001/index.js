0b432b45827ecdfc3f0db28249099db7b2706774
Jonathan Vivero <jonathanvivero@gmail.com>
2025-11-27 10:58:41 +0100

    phase 001 of development classes

---
+++ 001/index.js
@@ -0,0 +1,12 @@
+var number1 = 3;
+var number2 = 2;
+var number3 = 4;
+var number4 = 5;
+var number5 = 6;
+var number6 = 7;
+function sumar(n1, n2) {
+    return n1 + n2;
+}
+console.log(sumar(number1, number2));
+//console.log(sumar());
+//console.log(sumar());
---
+++ 001/index.ts
@@ -0,0 +1,14 @@
+let number1 = 3;
+let number2 = 2;
+let number3 = 4;
+let number4 = 5;
+let number5 = 6;
+let number6 = 7;
+
+function sumar(n1: any, n2: any): number {    
+    return n1 + n2;
+}
+
+console.log(sumar(number1, number2));
+//console.log(sumar());
+//console.log(sumar());
