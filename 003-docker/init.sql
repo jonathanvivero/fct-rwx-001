170812992db60c55e68e7d8dadd1cb282e799bfc
dasurih <seoaznar@gmail.com>
2025-12-07 22:07:44 +0100

    Create sql

---
+++ 003-docker/init.sql
@@ -0,0 +1,15 @@
+-- Crear tabla users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+-- Datos 
+INSERT INTO users (username, password, email) VALUES
+('admin', 'ad123', 'admin@test.com'),
+('rosario', 'ro456', 'rosario@test.com');
+('aznar', 'az789', 'aznar@test.com');
+('dev', 'dev10', 'dev@test.com');
+
